e8a23b9544a73d2c7c470344804b9fa371a8fc1d
32bitdev <32bitdev@gmail.com>
2023-06-27 20:02:02 +0530

    Server Modules and Response Headers Added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
 const { Users } = require("./collecctions/mongoCollections");
 const userRoutes = require("./routes/userRoutes");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 const app = express();
 require("dotenv").config();
 
 app.use(express.json());
+app.use(fileUpload());
+app.use(cookieParser());
+app.use(session({
+  secret: "secret",
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Authorization, authorization, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  next();
+});
 app.use("/auth", userRoutes);
 
 const server = app.listen(process.env.PORT, () =>
