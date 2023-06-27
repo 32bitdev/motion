e2b0faf769a252eb395b41a8872eca43f7ce5811
32bitdev <32bitdev@gmail.com>
2023-06-27 11:25:07 +0530

    Login RestAPI & Server Route Added

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -23,3 +23,21 @@ module.exports.register = async (req, res, next) => {
         next(ex);
     }
 };
+
+
+// login post request handler
+module.exports.login = async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+        const user = await Users.findOne({ username: username });
+        if (!user)
+            return res.status(400).json({ status: false, msg: "Incorrect Username or Password" });
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid)
+            return res.status(400).json({ status: false, msg: "Incorrect Username or Password" });
+        delete user.password;
+        return res.status(200).json({ status: true, user: user });
+    } catch (ex) {
+        next(ex);
+    }
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,4 +2,5 @@
 const { register } = require("../controllers/userControllers");
 const router = require("express").Router();
 router.post("/register", register);
+router.post("/login", login);
 module.exports = router;
