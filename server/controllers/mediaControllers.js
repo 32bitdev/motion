00d867590e5d46a10e138a0f2bf46bf5bbad742a
ArchanaY1203 <ay1840450@gmail.com>
2023-06-27 23:02:09 +0530

    ffmpeg/hls Converter Added

--- server/controllers/mediaControllers.js
+++ server/controllers/mediaControllers.js
@@ -1,6 +1,9 @@
 const { Users, Metadata, ObjectId } = require("../collections/mongoCollections");
 const path = require("path");
 const fs = require("fs");
+const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
+const ffmpeg = require("fluent-ffmpeg");
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
 //upload request handler
 module.exports.upload = async (req, res, next) => {
@@ -35,6 +38,32 @@ module.exports.upload = async (req, res, next) => {
             if (err) {
                 return res.status(500).json({ status: false, msg: "Something went wrong" });
             }
+            const update = await Users.updateOne({ _id: new ObjectId(_id) }, { $set: { mediaCount: newMediaCount } });
+            if (!update.acknowledged)
+                return res.status(500).json({ status: false, msg: "Something went wrong" });
+            const metaAdd = await Metadata.insertOne({ videoId: videoId, title: title, description: description, isPrivate: true, owner: _id, ownerName: user.username, processed: false });
+            if (!metaAdd.acknowledged)
+                return res.status(500).json({ status: false, msg: "Something went wrong" });
+            console.log(`Video processing started - ${videoId}`);
+            ffmpeg(`${path.join(__dirname, `..`, `..`, `database`, `${file.name}.mp4`)}`).addOptions([
+                '-profile:v main',
+                '-vf scale=w=1280:h=720:force_original_aspect_ratio=decrease',
+                '-c:a aac',
+                '-ar 48000',
+                '-b:a 128k',
+                '-c:v h264',
+                '-crf 20',
+                '-g 48',
+                '-keyint_min 48',
+                '-sc_threshold 0',
+                '-b:v 2800k',
+                '-maxrate 2996k',
+                '-bufsize 4200k',
+                '-hls_time 10',
+                `-hls_segment_filename ${path.join(__dirname, `..`, `..`, `database`, `${videoId}_720p_%04d.ts`)}`,
+                '-hls_playlist_type vod',
+                '-f hls'
+            ]).output(`${path.join(__dirname, `..`, `..`, `database`, `${file.name}.m3u8`)}`).on('end', processingDone).run()
             return res.status(200).json({ status: true, msg: "Upload successfull", videoId: videoId });
         });
     }
