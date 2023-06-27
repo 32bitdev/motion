286370904ae7df6803b19b9b2936a4b840439e27
32bitdev <32bitdev@gmail.com>
2023-06-27 21:19:46 +0530

    MongoDB Database Collections Updated

--- server/collections/mongoCollections.js
+++ server/collections/mongoCollections.js
@@ -1,8 +1,9 @@
 //mongodb collections configuration
 require("dotenv").config();
-const { MongoClient } = require("mongodb");
+const { ObjectId, MongoClient } = require("mongodb");
 const client = new MongoClient(process.env.MONGO_URL);
 const db = client.db("motion");
 client.connect();
 const Users = db.collection("users");
-module.exports = { Users };
+const Metadata = db.collection("metaDatas");
+module.exports = { Users, Metadata, ObjectId };
