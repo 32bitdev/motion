70377e695127efc63f22ba0f0f44ff46b30c3e45
ArchanaY1203 <ay1840450@gmail.com>
2023-06-29 15:19:59 +0530

    Server Routes Added

--- server/routes/mediaRoutes.js
+++ server/routes/mediaRoutes.js
@@ -1,9 +1,10 @@
 //server routes to handle get and post requests
-const { upload, getDetails, changeVisibility, getThumbs, download } = require("../controllers/mediaControllers");
+const { upload, getDetails, changeVisibility, getVideos, getThumbs, download } = require("../controllers/mediaControllers");
 const router = require("express").Router();
 router.post("/upload", upload);
 router.post("/getDetails", getDetails);
 router.post("/changeVisibility", changeVisibility);
+router.post("/getVideos", getVideos);
 router.post("/download", download);
 router.get("/getThumbs/:videoId", getThumbs);
 module.exports = router;
