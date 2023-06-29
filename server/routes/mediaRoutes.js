dfb9ff183eb21f9d133ac046c33d7b6a9b4a12a7
32bitdev <32bitdev@gmail.com>
2023-06-29 13:05:46 +0530

    Server Routes Updated

--- server/routes/mediaRoutes.js
+++ server/routes/mediaRoutes.js
@@ -1,6 +1,9 @@
 //server routes to handle get and post requests
-const { upload, getDetails } = require("../controllers/mediaControllers");
+const { upload, getDetails, changeVisibility, getThumbs, download } = require("../controllers/mediaControllers");
 const router = require("express").Router();
 router.post("/upload", upload);
 router.post("/getDetails", getDetails);
+router.post("/changeVisibility", changeVisibility);
+router.post("/download", download);
+router.get("/getThumbs/:videoId", getThumbs);
 module.exports = router;
