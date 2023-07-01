5fcd2e9c6331c6fcce54d1602a23586e69685c32
ArchanaY1203 <ay1840450@gmail.com>
2023-07-01 23:19:14 +0530

    Frontend Routes Added

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import Upload from "./pages/Upload";
+import Profile from "./pages/Profile";
+import Details from "./pages/Details";
+import Player from "./pages/Player";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/upload" element={<Upload />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/details/:videoId" element={<Details />} />
+        <Route path="/player/:videoId" element={<Player />} />
       </Routes>
     </BrowserRouter >
   )
