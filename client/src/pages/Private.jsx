54c39b2d237aa98a5b0b507a2142c415aa718ccd
ArchanaY1203 <ay1840450@gmail.com>
2023-07-09 12:16:41 +0530

    Private Page Added

---
+++ client/src/pages/Private.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../css/Private.css";
+
+export default function Private() {
+  return (
+    <div className="private">
+      Private Content
+    </div>
+  )
+}
